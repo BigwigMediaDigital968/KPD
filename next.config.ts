49256c624a377edcf63c96257f1e9810ffdaec26
shubham9049 <shubham.rajveer19@gmail.com>
2025-12-15 15:54:04 +0530

    add changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "khalsaproperty.in"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/blogs/how-to-buy-property-in-dubai-as-an-indian-citizen",
+        destination: "/",
+        permanent: true, // 301 SEO redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
